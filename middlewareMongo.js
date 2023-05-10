f5fc314b4c62f9599d54ffcab0182a97c6971613
danielmdavis <danis1911@gmail.com>
2023-05-10 14:26:28 -0400

    clone

---
+++ middlewareMongo.js
@@ -0,0 +1,98 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+
+const uri = 'mongodb+srv://admin:eternity1@cluster0.3az9hbx.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    connectTimeoutMS: 10000, 
+    serverApi: ServerApiVersion.v1 
+})
+
+async function getter() {
+    await client.connect()
+    const db = client.db('db')
+    let returns = await db.collection('propositions').find()
+    let data = []
+    await returns.forEach(each => data.push(each))
+    data = JSON.stringify(data)
+    return data
+}
+
+async function onePropoGetter(id) {
+    await client.connect()
+    const db = client.db('db')
+    let data = await db.collection('propositions').findOne({ 'id': id })
+    data = JSON.stringify(data)
+    return data
+}
+
+async function oneAddendumGetter(id, id2) {
+    await client.connect()
+    const db = client.db('db')
+    const proposition = await db.collection('propositions').findOne({ 'id': id })
+    const addendum = JSON.stringify(proposition.addenda)
+    return addendum
+}
+
+async function onePropoUpvoter(id) {
+    await client.connect()
+    const db = client.db('db')
+    await db.collection('propositions').updateOne(
+        { 'id': id },
+        { $inc: {  'ups': 1 } }
+    )
+}
+async function onePropoDownvoter(id) {
+    await client.connect()
+    const db = client.db('db')
+    await db.collection('propositions').updateOne(
+        { 'id': id },
+        { $inc: {  'downs': 1 } }
+    )
+}
+
+async function oneAddendumUpvoter(id, id2) {
+    await client.connect()
+    const db = client.db('db')
+    await db.collection('propositions').updateOne(
+        { 'id': id },
+        { $inc: { 'addenda.$[add].ups' : 1 } },
+        { arrayFilters: [ { 'add.id': { $eq: id2 } } ] }
+     )
+}
+async function oneAddendumDownvoter(id, id2) {
+    await client.connect()
+    const db = client.db('db')
+    await db.collection('propositions').updateOne(
+        { 'id': id },
+        { $inc: { 'addenda.$[add].downs' : 1 } },
+        { arrayFilters: [ { 'add.id': { $eq: id2 } } ] }
+     )
+}
+
+async function propoPoster(payload) {
+    await client.connect()
+    const db = client.db('db')
+    db.collection('propositions').insertOne(payload)
+}
+    
+async function addendumPoster(id, payload) {
+    await client.connect()
+    const db = client.db('db')
+    const foo = await db.collection('propositions').updateOne(
+        { 'id': parseInt(id) }, 
+        { $push: { 'addenda': payload } }
+    )
+}
+
+exports.getter = getter
+exports.onePropoGetter = onePropoGetter 
+exports.oneAddendumGetter = oneAddendumGetter 
+exports.onePropoUpvoter = onePropoUpvoter 
+exports.onePropoDownvoter = onePropoDownvoter 
+exports.oneAddendumUpvoter = oneAddendumUpvoter 
+exports.oneAddendumDownvoter = oneAddendumDownvoter 
+exports.propoPoster = propoPoster
+exports.addendumPoster = addendumPoster
---
+++ postgres/middlewarePostgres.js
@@ -0,0 +1,60 @@
+const { Client } = require("pg");
+require('dotenv').config();
+
+const credentials = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    ssl: false
+}
+
+async function getCount() {
+  const client = new Client(credentials)
+  await client.connect()
+  const data = await client.query('SELECT COUNT(*) FROM propositions;')
+  await client.end()
+  return data
+}
+
+async function getAllProps() {
+  const client = new Client(credentials)
+  await client.connect()
+  const data = await client.query('SELECT * FROM propositions;')
+  await client.end()
+  return data
+}
+
+async function getOneProp(id) {
+  const client = new Client(credentials)
+  await client.connect()
+  const data = await client.query('SELECT * FROM propositions WHERE propositions.id =\''+id+'\';')
+  await client.end()
+  return data
+}
+
+async function getOnePropWithChildren(id) {
+  const client = new Client(credentials)
+  await client.connect()
+  const proposition = await client.query('SELECT * FROM propositions WHERE propositions.id =\''+id+'\';')
+  const addenda = await client.query('SELECT * FROM addenda WHERE addenda.prop_id =\''+id+'\';')
+  await client.end()
+  return [proposition, addenda]
+}
+
+async function getAddenda() {
+  const client = new Client(credentials)
+  await client.connect()
+  const data = await client.query('SELECT * FROM propositions INNER JOIN addenda ON propositions.id = addenda.prop_id;')
+  await client.end()
+  return data
+}
+
+exports.getCount = getCount
+exports.getAllProps = getAllProps
+exports.getOneProp = getOneProp
+exports.getOnePropWithChildren = getOnePropWithChildren
+exports.getAddenda = getAddenda
+
+// not implemented in mvp
